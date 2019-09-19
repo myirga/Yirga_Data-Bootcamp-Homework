d122d90c41c9863bb70696e47e653f3c2e768d24
Mina Y <minayirga@gmail.com>
2019-09-18 23:07:42 -0400

    submitting homework

--- HW 14_JavaScript/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
+++ HW 14_JavaScript/14-Intro-To-JavaScript/Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,20 @@
 // from data.js
 var tableData = data;
+var button = d3.select("#filter-btn");
+var inputField1 = d3.select("#datetime");
+var inputField2 = d3.select("#city");
+var tbody = d3.select("tbody");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+var populate = (dataInput) => {
 
-// YOUR CODE HERE!
+    dataInput.forEach(ufo_sightings => {
+      var row = tbody.append("tr");
+      columns.forEach(column => row.append("td").text(ufo_sightings[column])
+      )
+    });
+  }
+  
+  //Populate table
+  populate(data);
+  
